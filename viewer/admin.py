6a88600308f94866caae1e2403a754ee4ead28e8
Shharki <andrej.daniel@seznam.cz>
2025-07-10 03:19:17 +0200

    Model Artist.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from viewer.models import Genre, Country
+from viewer.models import Genre, Country, Artist
 
 admin.site.register(Genre)
 admin.site.register(Country)
+admin.site.register(Artist)
--- viewer/models.py
+++ viewer/models.py
@@ -1,5 +1,4 @@
-from django.db import models
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, DateField, ForeignKey, TextField, DateTimeField, SET_NULL
 
 
 class Genre(Model):
@@ -27,3 +26,26 @@ class Country(Model):
 
     def __str__(self):
         return self.name
+
+
+class Artist(Model):
+    name = CharField(max_length=32, null=True, blank=True)
+    surname = CharField(max_length=32, null=True, blank=True)
+    artistic_name = CharField(max_length=32, null=True, blank=True)
+    date_of_birth = DateField(null=True, blank=True, unique=True)
+    date_of_death = DateField(null=True, blank=True, unique=True)
+    country = ForeignKey(Country, null=True, blank=True, on_delete=SET_NULL, related_name='artists')
+    biography = TextField(null=True, blank=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['surname', 'name']
+
+    def __repr__(self):
+        return f"Artist(name={self.name}, surname={self.surname})"
+
+    def __str__(self):
+        return self.artistic_name or f"{self.name} {self.surname}"
+
