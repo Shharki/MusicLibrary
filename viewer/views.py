5bae2dca2f52a9fb55de37c4153cf06fdafcd2ea
jkstep <jakubkrocil@gmail.com>
2025-07-22 17:04:49 +0200

    Feature 1 - list of songs.

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from viewer.models import Song
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def songs(request):
+    songs_list = Song.objects.all()
+    context = {'songs': songs_list}
+    return render(request, 'songs.html', context)
